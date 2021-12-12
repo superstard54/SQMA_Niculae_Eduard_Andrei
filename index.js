d63e0642f3a35274c730d3c2f3b519c1f1432ec1
unknown <niculae.eduard97@yahoo.com>
2021-12-12 12:11:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let getSum = function getSum(a,b){
+    return a+b;
+}
+
+let getDifference = function(a,b){
+    return a-b;
+}
+
+export default  {
+    getSum,
+    getDifference
+}
+
+
---
+++ testSum.js
@@ -0,0 +1,24 @@
+import  * as data  from "./index.js";
+
+let testResults = [];
+
+function testCases(cases) {
+  cases.forEach((element) => {
+    let result = data.default.getSum(element[0], element[1]);
+    if (result !== element[2]) {
+      testResults.push(
+        `For values ${element[0]} and ${element[1]} the expected result is ${element[2]} but got ${result}`
+      );
+      return;
+    }
+    testResults.push("OK");
+  });
+}
+
+let cases = [
+  [1, 2, 3],
+  [5, 4, 10],
+];
+
+testCases(cases);
+console.log(testResults);
